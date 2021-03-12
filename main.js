24f9b814b12c5a6cf3bc78b3e3e344d740efb530
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-03-11 20:28:43 -0800

    hard-coding the visibility attribute isn't dry but it works, home still takes up space.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const home = document.getElementById('home'); 
 const about = document.getElementById('about'); 
 
-
 // LINK VARIABLES
 const homeLink = document.getElementById('home-link')
 const aboutLink = document.getElementById('about-link')
@@ -12,8 +11,20 @@ const navigate = (element) => {
     element.style.visibility = 'visible'
 }
 
+
 // EVENT LISTENERS
 // homeLink.addEventListener('click', navigate(home))
 aboutLink.addEventListener('click', function() {
+    home.style.visibility = 'hidden'
     navigate(about)
 })
+
+
+/* 
+DEV NOTES: 
+Hard coding isn't dry but it works. 
+
+the only issue is that the home element still takes up sapce. 
+
+
+*/
