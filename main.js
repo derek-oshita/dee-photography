215314e985138595c76e56db1d8155198f83e25c
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-09-15 22:21:29 -0700

    Need to conditionally enable the submit button when all flags are true.

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const form = document.getElementById("input-form"),
   nameField = document.getElementById("input-name"),
   emailField = document.getElementById("input-email"),
   subjectField = document.getElementById("input-subject"),
-  messageFieldd = document.getElementById("input-message"),
+  messageField = document.getElementById("input-message"),
   submitBtn = document.getElementById("submit-btn");
 
 const invalidNameDiv = document.getElementById("invalid-name"),
   invalidEmailDiv = document.getElementById("invalid-email"),
-  invalidSubjectDiv = document.getElementById("invalid-subject");
+  invalidSubjectDiv = document.getElementById("invalid-subject"),
+  invalidMessageDiv = document.getElementById("invalid-message");
 
 let nameFieldIsValid = false,
   emailFieldIsValid = false,
@@ -19,21 +20,72 @@ let nameFieldIsValid = false,
 const invalidText = "Please enter at least 2 characters",
   invalidEmail = "Please enter a valid email address";
 
-const disableSubmitOnEmptyFields = () => {
-  if (
-    !nameField.value ||
-    !emailField.value ||
-    !subjectField.value ||
-    !messageFieldd.value
-  ) {
-    submitBtn.disabled = true;
+const checkInputField = (field, value) => {
+  field === emailField
+    ? toggleEmailNotification(value)
+    : toggleTextNotification(value, field);
+};
+
+const toggleEmailNotification = (value) => {
+  if (value.length < 5 || !value.includes("@")) {
+    invalidEmailDiv.style.display = "block";
+    invalidEmailDiv.innerHTML = invalidEmail;
+    console.log("emailField", emailFieldIsValid);
+  } else {
+    invalidEmailDiv.style.display = "none";
+    emailFieldIsValid = true;
+    console.log("emailField", emailFieldIsValid);
   }
 };
 
-const checkInputField = (field, value) => {
-  field === emailField
-    ? console.log("email") // toggleEmailNotification
-    : console.log("other"); // toggleTextNotifcation
+const toggleTextNotification = (value, field) => {
+  if (value.length < 2) {
+    switch (field) {
+      case nameField:
+        invalidNameDiv.style.display = "block";
+        invalidNameDiv.innerHTML = invalidText;
+        nameFieldIsValid = false;
+      case subjectField:
+        invalidSubjectDiv.style.display = "block";
+        invalidSubjectDiv.innerHTML = invalidText;
+        subjectFieldIsValid = false;
+      case messageField:
+        invalidMessageDiv.style.display = "block";
+        invalidMessageDiv.innerHTML = invalidText;
+        messageFieldIsValid = false;
+    }
+  } else {
+    switch (field) {
+      case nameField:
+        invalidNameDiv.style.display = "none";
+        nameFieldIsValid = true;
+        console.log("nameField", nameFieldIsValid);
+        break;
+      case subjectField:
+        invalidSubjectDiv.style.display = "none";
+        subjectFieldIsValid = true;
+        console.log("subjectField", subjectFieldIsValid);
+        break;
+      case messageField:
+        invalidMessageDiv.style.display = "none";
+        messageFieldIsValid = true;
+        console.log("messageField", messageFieldIsValid);
+        break;
+    }
+  }
+};
+
+const canSubmit = () => {
+  if (
+    nameFieldIsValid === true &&
+    emailFieldIsValid === true &&
+    subjectFieldIsValid === true &&
+    messageFieldIsValid === true
+  ) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
 };
 
 // NAME FIELD
@@ -48,10 +100,20 @@ emailField.addEventListener("input", () => {
   checkInputField(emailField, emailVal);
 });
 
-// INPUT FIELD
+// SUBJECT FIELD
 subjectField.addEventListener("input", () => {
   let subjectVal = subjectField.value;
   checkInputField(subjectField, subjectVal);
 });
 
-disableSubmitOnEmptyFields();
+// MESSAGE FIELD
+messageField.addEventListener("input", () => {
+  let messageVal = messageField.value;
+  checkInputField(messageField, messageVal);
+});
+
+submitBtn.addEventListener("click", () => {
+  console.log("clicked");
+});
+
+// need to conditionally enable the submit button when all of the flags are true
