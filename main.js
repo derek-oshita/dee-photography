fc65f78d3a18ff1907260a89b136d03bc1b2dde5
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-09-15 20:17:07 -0700

    Added custom validation message for name and email input field.

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ const form = document.getElementById("input-form"),
   messageFieldd = document.getElementById("input-message"),
   submitBtn = document.getElementById("submit-btn");
 
+const invalidNameDiv = document.getElementById("invalid-name"),
+  invalidEmailDiv = document.getElementById("invalid-email");
+
+let nameFieldIsValid = false,
+  emailFieldIsValid = false,
+  subjectFieldIsValid = false,
+  messageFieldIsValid = false;
+
+const invalidText = "Please enter at least 2 characters",
+  invalidEmail = "Please enter a valid email address";
+
 const disableSubmitOnEmptyFields = () => {
   if (
     !nameField.value ||
@@ -18,8 +29,34 @@ const disableSubmitOnEmptyFields = () => {
   }
 };
 
-submitBtn.addEventListener("click", () => {
-  console.log("clicked");
+// NAME FIELD
+nameField.addEventListener("input", () => {
+  let nameVal = nameField.value;
+
+  if (nameVal.length < 2) {
+    invalidNameDiv.style.display = "block";
+    invalidNameDiv.innerHTML = invalidText;
+  }
+
+  if (nameVal.length >= 2) {
+    invalidNameDiv.style.display = "none";
+    nameFieldIsValid = true;
+  }
+});
+
+// EMAIL FIELD
+emailField.addEventListener("input", () => {
+  let emailVal = emailField.value;
+
+  if (emailVal.length < 2) {
+    invalidEmailDiv.style.display = "block";
+    invalidEmailDiv.innerHTML = invalidEmail;
+  }
+
+  if (emailVal.length >= 2 && emailVal.includes("@")) {
+    invalidEmailDiv.style.display = "none";
+    emailFieldIsValid = true;
+  }
 });
 
 disableSubmitOnEmptyFields();
