5303e85e907009f5be0e63378c1815476862f98e
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-09-19 15:13:51 -0700

    Finished adding photos to personal page.

--- main.js
+++ main.js
@@ -125,3 +125,10 @@ messageField.addEventListener("input", () => {
 });
 
 disableSubmitBtnOnLoad();
+console.log(nameField);
+
+for (let i = 1; i < 100; i++) {
+  if (i % 3 === 0 && i % 4 === 0) {
+    console.log(i);
+  }
+}
