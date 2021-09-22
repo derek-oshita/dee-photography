9b72e84379e410cae5e2bd7f632f99940031d80b
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-09-21 20:28:10 -0700

    Removed logs from main.js

--- main.js
+++ main.js
@@ -125,10 +125,3 @@ messageField.addEventListener("input", () => {
 });
 
 disableSubmitBtnOnLoad();
-console.log(nameField);
-
-for (let i = 1; i < 100; i++) {
-  if (i % 3 === 0 && i % 4 === 0) {
-    console.log(i);
-  }
-}
